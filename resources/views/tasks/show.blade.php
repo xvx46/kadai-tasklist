d8222d05c7e569e9d1b639e0bc9de6c28cd3a129
yu.yama <yu.yama@a-tm.com>
2018-05-22 09:33:56 +0000

    commit

---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>id = {{ $task->id }} のメッセージ詳細ページ</h1>
+
+    <p>ステータス: {{ $task->status }}</p>
+    <p>タスク：{{ $task->content }}</p>
+
+    {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id]) !!}
+
+    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除') !!}
+    {!! Form::close() !!}
+
+@endsection
