3f190e185ad401b273940b08cce4cafec4717f18
yu.yama <yu.yama@a-tm.com>
2018-05-23 05:56:07 +0000

    kadai naoshi

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -5,8 +5,7 @@
 <h1>id: {{ $task->id }} のタスク編集ページ</h1>
 
 <div class="row">
-    <div class="col-sm-8 col-sm-offset-2>
-    <div class="col-lg-6 col-lg-offset-3>
+    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
         
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
          <div class="form-group">
@@ -21,7 +20,6 @@
         {!! Form::submit('投稿', ['class' => 'btn btn-default']) !!}
 
     {!! Form::close() !!}
-    </div>
     </div>
 </div>
     
